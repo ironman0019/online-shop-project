7c47a697d824d1092c5e6717593425e794853495
Emad Isavi <emadesavi@gmail.com>
2024-12-10 19:40:22 +0330

    Add admin menu, faq & page resource routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Admin\Content\FaqController;
+use App\Http\Controllers\Admin\Content\MenuController;
+use App\Http\Controllers\Admin\Content\PageController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,14 +10,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 require __DIR__.'/auth.php';
+
+Route::prefix('admin')->name('admin.')->group(function() {
+
+    Route::prefix('content')->name('content.')->group(function() {
+        Route::resource('menu', MenuController::class);
+        Route::resource('faq', FaqController::class);
+        Route::resource('page', PageController::class);
+    });
+
+});
