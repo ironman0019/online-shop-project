cbc561bbf0ec3a6295b78710e7e5543a2ed31480
Emad Isavi <emadesavi@gmail.com>
2024-12-11 18:19:34 +0330

    Add comment and tickets routes

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Admin\Content\FaqController;
 use App\Http\Controllers\Admin\Content\MenuController;
 use App\Http\Controllers\Admin\Content\PageController;
-use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\Ticket\TicketController;
+use App\Http\Controllers\Admin\Content\CommentController;
+use App\Http\Controllers\Admin\Ticket\TicketAdminController;
+use App\Http\Controllers\Admin\Ticket\TicketCategoryController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,6 +23,14 @@ Route::prefix('admin')->name('admin.')->group(function() {
         Route::resource('menu', MenuController::class);
         Route::resource('faq', FaqController::class);
         Route::resource('page', PageController::class);
+        Route::resource('comment', CommentController::class);
+    });
+
+    Route::prefix('tickets')->name('tickets.')->group(function() {
+        Route::resource('ticket-category', TicketCategoryController::class);
+        Route::resource('ticket-admin', TicketAdminController::class);
+        Route::resource('ticket', TicketController::class);
+
     });
 
 });
