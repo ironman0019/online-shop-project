601fa35a5c5b5d98422ac9b076c2b6c4eb659b29
Emad Isavi <emadesavi@gmail.com>
2024-12-11 18:17:23 +0330

    Create ticket admin controller

---
+++ app/Http/Controllers/Admin/Ticket/TicketAdminController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Ticket;
+
+use App\Http\Controllers\Controller;
+use App\Models\Ticket\TicketAdmin;
+use Illuminate\Http\Request;
+
+class TicketAdminController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TicketAdmin $ticketAdmin)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TicketAdmin $ticketAdmin)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, TicketAdmin $ticketAdmin)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TicketAdmin $ticketAdmin)
+    {
+        //
+    }
+}
