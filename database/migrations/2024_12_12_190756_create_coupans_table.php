94849338f30b29ea2a8d49c78841652723a0f395
Emad Isavi <emadesavi@gmail.com>
2024-12-13 00:20:34 +0330

    Create coupan model and migration

---
+++ database/migrations/2024_12_12_190756_create_coupans_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('coupans', function (Blueprint $table) {
+            $table->id();
+            $table->string('code');
+            $table->string('amount');
+            $table->tinyInteger('amount_type')->default(0)->comment('0 => %, 1 => unit');
+            $table->unsignedBigInteger('discount_ceiling')->nullable();
+            $table->timestamp('start_date')->useCurrent();
+            $table->timestamp('end_date')->useCurrent();
+            $table->tinyInteger('status')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('coupans');
+    }
+};
