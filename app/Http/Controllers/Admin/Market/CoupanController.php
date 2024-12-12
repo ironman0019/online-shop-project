0dd83f6bff04fc9724aeca8a0aa7fa8be291f29c
Emad Isavi <emadesavi@gmail.com>
2024-12-13 00:20:53 +0330

    Create coupan controller

---
+++ app/Http/Controllers/Admin/Market/CoupanController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Market;
+
+use App\Http\Controllers\Controller;
+use App\Models\Market\Coupan;
+use Illuminate\Http\Request;
+
+class CoupanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Coupan $coupan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Coupan $coupan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Coupan $coupan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Coupan $coupan)
+    {
+        //
+    }
+}
