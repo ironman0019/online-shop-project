96a42e9d85392c7be27abce86f5590b7e672e387
Emad Isavi <emadesavi@gmail.com>
2024-12-12 22:35:22 +0330

    Create product model and migration

---
+++ database/migrations/2024_12_12_182421_create_products_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->string('slug')->unique()->nullable();
+            $table->text('image');
+            $table->decimal('weight', 10, 2)->nullable();
+            $table->decimal('length', 10, 2)->nullable();
+            $table->decimal('width', 10, 2)->nullable();
+            $table->decimal('height', 10, 2)->nullable();
+            $table->bigInteger('price');
+            $table->text('tags');
+            $table->bigInteger('sold_number');
+            $table->bigInteger('marketable_number');
+            $table->tinyInteger('marketable')->default(0);
+            $table->tinyInteger('status')->default(0);
+            $table->foreignId('brand_id')->constrained('brands')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('product_category_id')->constrained('product_categories')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
