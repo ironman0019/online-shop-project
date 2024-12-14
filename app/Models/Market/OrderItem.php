8399504baebc21448fab58ff9d5445135baacee1
Emad Isavi <emadesavi@gmail.com>
2024-12-14 23:33:02 +0330

    Create order item model and migration

---
+++ app/Models/Market/OrderItem.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Market;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class OrderItem extends Model
+{
+    use SoftDeletes;
+
+    protected $guarded = ['id'];
+}
---
+++ database/migrations/2024_12_14_170429_create_order_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained('orders')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade')->onUpdate('cascade');
+            $table->text('product_object')->nullable();
+            $table->integer('quantity')->default(1);
+            $table->unsignedBigInteger('price');
+            $table->unsignedBigInteger('total_price');
+            $table->tinyInteger('status')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_items');
+    }
+};
