aaa7f82bc619deaccf6944278d95acce84252a54
Emad Isavi <emadesavi@gmail.com>
2025-01-01 03:13:31 +0330

    Use sluggable in product category

--- app/Models/Market/ProductCategory.php
+++ app/Models/Market/ProductCategory.php
@@ -2,12 +2,27 @@
 
 namespace App\Models\Market;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProductCategory extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, Sluggable;
 
     protected $guarded = ['id'];
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
 }
