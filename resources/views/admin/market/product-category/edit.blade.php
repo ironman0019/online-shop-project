cd824e4cf1f75180bc1f2620b0901ed412da0602
Emad Isavi <emadesavi@gmail.com>
2025-01-01 03:16:30 +0330

    Create edit product category page

---
+++ resources/views/admin/market/product-category/edit.blade.php
@@ -0,0 +1,62 @@
+@extends('admin.layouts.master')
+
+@section('title', 'ویرایش دسته بندی محصولات')
+
+@section('content')
+
+
+
+    <section class="row">
+        <section class="col-12">
+            <section class="main-body-container">
+                <section class="main-body-container-header d-flex justify-content-between align-items-center">
+                    <div>
+                        <h5>
+                            ویرایش دسته بندی
+                        </h5>
+
+                    </div>
+                    <div>
+                        <a href="{{ route('admin.market.product-category.index') }}" class="btn btn-warning">بازگشت</a>
+                    </div>
+                </section>
+                <section class="body-content">
+
+                    <form class="row g-3" action="{{ route('admin.market.product-category.update', $productCategory) }}" method="post">
+                        @csrf
+                        @method('PUT')
+
+                        <div class="col-md-6 mb-2">
+                            <label for="name" class="form-label">نام</label>
+                            <input type="text" name="name" class="form-control" id="name"
+                                value="{{ old('name', $productCategory->name) }}">
+                            @error('name')
+                                <small class="text-danger">{{ $message }}</small>
+                            @enderror
+                        </div>
+                        <div class="col-md-6 mb-2">
+                            <label for="status" class="form-label">وضعیت</label>
+                            <select class="form-control" name="status" id="status">
+                                <option value="1" @if (old('status', $productCategory->status) == 1) selected @endif>فعال</option>
+                                <option value="0" @if (old('status', $productCategory->status) == 0) selected @endif>غیر فعال</option>
+                            </select>
+                            @error('status')
+                                <small class="text-danger">{{ $message }}</small>
+                            @enderror
+                        </div>
+
+
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-success">ثبت</button>
+                        </div>
+                    </form>
+
+                </section>
+            </section>
+        </section>
+    </section>
+
+
+
+
+@endsection
