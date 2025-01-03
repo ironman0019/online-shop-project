a39aacf1768783f36686337a2a7114ef7787966e
Emad Isavi <emadesavi@gmail.com>
2025-01-04 02:28:40 +0330

    Add sluggable to page model

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -3,11 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Page extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, Sluggable;
 
     protected $guarded = ['id'];
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 }
