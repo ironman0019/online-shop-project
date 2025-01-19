ad4920e64a40a32be1f5b5f22916a1ce7b3a0ff6
Emad Isavi <emadesavi@gmail.com>
2025-01-20 01:12:03 +0330

    Add user, parent & children and commentable relation to comment model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -10,4 +10,28 @@ class Comment extends Model
     use SoftDeletes;
 
     protected $guarded = ['id'];
+
+    // Relation with user
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo($this, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany($this, 'parent_id');
+    }
+
+    /**
+     * Get the parent commentable model (blog or product).
+     */
+    public function commentable()
+    {
+        return $this->morphTo();
+    }
 }
