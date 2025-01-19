27f18f27eefee4e0442be4ffa534404695dcfed1
Emad Isavi <emadesavi@gmail.com>
2025-01-20 01:20:59 +0330

    Add parent & children relations and add cascadeSoftDeletes trait to menu model

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -2,12 +2,25 @@
 
 namespace App\Models;
 
+use Dyrynda\Database\Support\CascadeSoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Menu extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, CascadeSoftDeletes;
 
     protected $guarded = ['id'];
+
+    protected $cascadeDeletes = ['children'];
+
+    public function parent()
+    {
+        return $this->belongsTo($this, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany($this, 'parent_id');
+    }
 }
