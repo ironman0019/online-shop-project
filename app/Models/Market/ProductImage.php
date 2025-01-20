e701bf8e6dd85c7e6d28169f09f079472fab0c3e
Emad Isavi <emadesavi@gmail.com>
2025-01-21 02:37:03 +0330

    Add relation with product in product-image model

--- app/Models/Market/ProductImage.php
+++ app/Models/Market/ProductImage.php
@@ -10,4 +10,10 @@ class ProductImage extends Model
     use SoftDeletes;
 
     protected $guarded = ['id'];
+
+    // Relation with product
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
