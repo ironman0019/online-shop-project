2e31315d44294050af246810b33696d0c630a19a
Emad Isavi <emadesavi@gmail.com>
2025-01-21 01:08:15 +0330

    Add sluggable trait and add brand and product category relation in product model

--- app/Models/Market/Product.php
+++ app/Models/Market/Product.php
@@ -2,12 +2,41 @@
 
 namespace App\Models\Market;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, Sluggable;
 
     protected $guarded = ['id'];
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+
+    // Relation with brand
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+
+    // Relation with product_category
+    public function productCategory()
+    {
+        return $this->belongsTo(ProductCategory::class);
+    }
+
 }
