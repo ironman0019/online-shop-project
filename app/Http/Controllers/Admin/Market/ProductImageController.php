4a85b35f528cfa0ab53e422fc90a93e1d115d280
Emad Isavi <emadesavi@gmail.com>
2025-01-21 02:36:30 +0330

    Implement CRUD functions in admin product-image controller

--- app/Http/Controllers/Admin/Market/ProductImageController.php
+++ app/Http/Controllers/Admin/Market/ProductImageController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Admin\Market;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Market\StoreProductImageRequest;
+use App\Http\Requests\Admin\Market\UpdateProductImageRequest;
+use App\Models\Market\Product;
 use App\Models\Market\ProductImage;
+use App\Services\ImageUploadService;
 use Illuminate\Http\Request;
 
 class ProductImageController extends Controller
@@ -11,49 +15,72 @@ class ProductImageController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Product $product)
     {
-        //
+        $productImages = ProductImage::where('product_id', $product->id)->get();
+        return view('admin.market.product-image.index', compact('product', 'productImages'));
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(Product $product)
     {
-        //
+        return view('admin.market.product-image.create', compact('product'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreProductImageRequest $request, ImageUploadService $imageUploadService, Product $product)
     {
-        //
+        $inputs = $request->all();
+
+        if($request->hasFile('image')) {
+            $result = $imageUploadService->uploadImage($request->file('image'));
+            if($result === false) {
+                return back()->with('swal-error', 'خطا در آپلود عکس');
+            }
+            $inputs['image'] = $result;
+        }
+
+        $inputs['product_id'] = $product->id;
+
+        ProductImage::create($inputs);
+        return to_route('admin.market.product-image.index', $product)->with('swal-success', 'عکس با موفقیت ساخته شد');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(ProductImage $productImage)
-    {
-        //
-    }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(ProductImage $productImage)
+    public function edit(ProductImage $productImage, Product $product)
     {
-        //
+        return view('admin.market.product-image.edit', compact('productImage', 'product'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, ProductImage $productImage)
+    public function update(UpdateProductImageRequest $request, ProductImage $productImage, Product $product, ImageUploadService $imageUploadService)
     {
-        //
+        $inputs = $request->all();
+
+        if($request->hasFile('image')) {
+            // Remove old image
+            $imageUploadService->removeImage($productImage->image);
+
+            $result = $imageUploadService->uploadImage($request->file('image'));
+            if($result === false) {
+                return back()->with('swal-error', 'خطا در آپلود عکس');
+            }
+            $inputs['image'] = $result;
+        }
+
+        $inputs['product_id'] = $product->id;
+
+        $productImage->update($inputs);
+        return to_route('admin.market.product-image.index', $product)->with('swal-success', 'عکس با موفقیت ویرایش شد');
     }
 
     /**
@@ -61,6 +88,7 @@ class ProductImageController extends Controller
      */
     public function destroy(ProductImage $productImage)
     {
-        //
+        $productImage->delete();
+        return back()->with('swal-success', 'عکس با موفقیت حذف شد');
     }
 }
