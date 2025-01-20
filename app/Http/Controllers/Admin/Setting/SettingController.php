0315faf1f960607b934f2e800b3f037236ac08b3
Emad Isavi <emadesavi@gmail.com>
2025-01-20 17:41:23 +0330

    Implement index, create and storeOrUpdate methods in admin setting controller

--- app/Http/Controllers/Admin/Setting/SettingController.php
+++ app/Http/Controllers/Admin/Setting/SettingController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Admin\Setting;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Setting\StoreOrUpdateSettingRequest;
 use App\Models\Setting\Setting;
+use App\Services\ImageUploadService;
 use Illuminate\Http\Request;
 
 class SettingController extends Controller
@@ -13,7 +15,8 @@ class SettingController extends Controller
      */
     public function index()
     {
-        //
+        $setting = Setting::first();
+        return view('admin.setting.site.index', compact('setting'));
     }
 
     /**
@@ -21,46 +24,71 @@ class SettingController extends Controller
      */
     public function create()
     {
-        //
+        $setting = Setting::first();
+        return view('admin.setting.site.set', compact('setting'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function storeOrUpdate(StoreOrUpdateSettingRequest $request, ImageUploadService $imageUploadService)
     {
-        //
+        $setting = Setting::first();
+        $inputs = $request->all();
+
+        // If setting exist we update the databse
+        if($setting) {
+
+            if($request->hasFile('logo')) {
+                // Remove old image
+                $imageUploadService->removeImage($setting->logo);
+    
+                $result = $imageUploadService->uploadImage($request->file('logo'), 'logo');
+                if($result === false) {
+                    return back()->with('swal-error', 'خطا در آپلود عکس');
+                }
+                $inputs['logo'] = $result;
+            }
+
+            if($request->hasFile('icon')) {
+                // Remove old image
+                $imageUploadService->removeImage($setting->icon);
+    
+                $result = $imageUploadService->uploadImage($request->file('icon'), 'icon');
+                if($result === false) {
+                    return back()->with('swal-error', 'خطا در آپلود عکس');
+                }
+                $inputs['icon'] = $result;
+            }
+    
+            $setting->update($inputs);
+            return to_route('admin.setting.index')->with('swal-success', 'تنظیمات با موفقیت ویرایش شد');
+        }
+
+        // else we store the data
+
+        else
+        {
+            if($request->hasFile('logo')) {
+                $result = $imageUploadService->uploadImage($request->file('logo'), 'logo');
+                if($result === false) {
+                    return back()->with('swal-error', 'خطا در آپلود عکس');
+                }
+                $inputs['logo'] = $result;
+            }
+
+            if($request->hasFile('icon')) {
+                $result = $imageUploadService->uploadImage($request->file('icon'), 'icon');
+                if($result === false) {
+                    return back()->with('swal-error', 'خطا در آپلود عکس');
+                }
+                $inputs['icon'] = $result;
+            }
+    
+            Setting::create($inputs);
+            return to_route('admin.setting.index')->with('swal-success', 'تنظیمات با موفقیت ساخته شد');
+        }
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Setting $setting)
-    {
-        //
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Setting $setting)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Setting $setting)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Setting $setting)
-    {
-        //
-    }
 }
