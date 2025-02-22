bb34a4bf21a58bcb8ee973a567c6942516203d90
Emad Isavi <emadesavi@gmail.com>
2025-02-23 00:35:41 +0330

    Create showcases table migration file

---
+++ database/migrations/2025_02_22_140831_create_showcases_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('showcases', function (Blueprint $table) {
+            $table->id();
+            $table->string('image');
+            $table->string('url');
+            $table->tinyInteger('position')->comment('0 => main slide show, 1 => top left, 2 => top left bottom');
+            $table->tinyInteger('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('showcases');
+    }
+};
