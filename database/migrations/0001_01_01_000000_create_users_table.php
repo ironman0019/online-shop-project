a21af7855b6d527a0f7fa6d0af35bdc8b7d43828
Emad Isavi <emadesavi@gmail.com>
2025-02-22 12:27:18 +0330

    Add nullable to name and password column in users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->string('email')->unique()->nullable();
             $table->string('mobile')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->timestamp('mobile_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->tinyInteger('activation')->default(0);
             $table->tinyInteger('is_admin')->default(0);
             $table->string('national_code')->nullable();
