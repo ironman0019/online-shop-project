5dbc8bc022b7bde823c8f629d0464d9e32431422
Emad Isavi <emadesavi@gmail.com>
2025-02-23 00:36:01 +0330

    Create showCase model

---
+++ app/Models/ShowCase.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ShowCase extends Model
+{
+    protected $table = 'showcases';
+
+    protected $guarded = ['id'];
+}
