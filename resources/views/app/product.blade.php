a4a42b6bf618f750b7aa0aadfd49ece2ce647a5c
Emad Isavi <emadesavi@gmail.com>
2025-02-23 13:49:36 +0330

    Add success session alert at top of the page, implement addToCart form, implement comment section in product view file

--- resources/views/app/product.blade.php
+++ resources/views/app/product.blade.php
@@ -15,6 +15,9 @@
                             <h2 class="content-header-title">
                                 <span>{{ $product->name }}</span>
                             </h2>
+                            @if(session('success'))
+                                <p class="alert alert-success">{{ session('success') }}</p>
+                            @endif
                             <section class="content-header-link">
                                 <!--<a href="#">مشاهده همه</a>-->
                             </section>
@@ -104,7 +107,11 @@
                                 </section>
 
                                 <section class="">
-                                    <a id="next-level" href="#" class="btn btn-danger d-block">افزودن به سبد خرید</a>
+                                    <form action="{{ route('cart.add', $product->id) }}" method="post">
+                                        @csrf
+                                        <button type="submit" id="next-level" href="#" class="btn btn-danger d-block w-100">افزودن به سبد خرید</button>
+                                    </form>
+                                    
                                 </section>
 
                             </section>
@@ -298,31 +305,22 @@
                                                         aria-label="Close"></button>
                                                 </section>
                                                 <section class="modal-body">
-                                                    <form class="row" action="#">
-
-                                                        <section class="col-6 mb-2">
-                                                            <label for="first_name" class="form-label mb-1">نام</label>
-                                                            <input type="text" class="form-control form-control-sm"
-                                                                id="first_name" placeholder="نام ...">
-                                                        </section>
-
-                                                        <section class="col-6 mb-2">
-                                                            <label for="last_name" class="form-label mb-1">نام
-                                                                خانوادگی</label>
-                                                            <input type="text" class="form-control form-control-sm"
-                                                                id="last_name" placeholder="نام خانوادگی ...">
-                                                        </section>
+                                                    <form class="row" action="{{ route('comment.create', $product) }}" method="POST" id="create-comment-form">
+                                                        @csrf
 
                                                         <section class="col-12 mb-2">
                                                             <label for="comment" class="form-label mb-1">دیدگاه
                                                                 شما</label>
-                                                            <textarea class="form-control form-control-sm" id="comment" placeholder="دیدگاه شما ..." rows="4"></textarea>
+                                                            <textarea class="form-control form-control-sm" id="comment" placeholder="دیدگاه شما ..." rows="4" name="body"></textarea>
+                                                            @error('body')
+                                                                <small class="text-danger">{{ $message }}</small>
+                                                            @enderror
                                                         </section>
 
                                                     </form>
                                                 </section>
                                                 <section class="modal-footer py-1">
-                                                    <button type="button" class="btn btn-sm btn-primary">ثبت
+                                                    <button type="submit" class="btn btn-sm btn-primary" form="create-comment-form">ثبت
                                                         دیدگاه</button>
                                                     <button type="button" class="btn btn-sm btn-danger"
                                                         data-bs-dismiss="modal">بستن</button>
@@ -331,59 +329,33 @@
                                         </section>
                                     </section>
                                 </section>
-
+                                @foreach($comments as $comment)
                                 <section class="product-comment">
+                                    @if(!$comment->parent_id)
                                     <section class="product-comment-header d-flex justify-content-start">
-                                        <section class="product-comment-date">۲۱ مرداد ۱۴۰۰</section>
-                                        <section class="product-comment-title">مجتبی مجدی</section>
+                                        <section class="product-comment-date">{{ \Morilog\Jalali\Jalalian::forge($comment->created_at)->format('%A, %d %B %y') }}</section>
+                                        <section class="product-comment-title">{{ $comment->user->name }}</section>
                                     </section>
                                     <section class="product-comment-body">
-                                        با این تخفیف قیمت خیلی خوبه
+                                        {{ $comment->body }}
                                     </section>
-                                </section>
+                                    @endif
 
-                                <section class="product-comment">
-                                    <section class="product-comment-header d-flex justify-content-start">
-                                        <section class="product-comment-date">۲۱ مرداد ۱۴۰۰</section>
-                                        <section class="product-comment-title">هدیه سادات هاشمی نژاد</section>
-                                    </section>
-                                    <section class="product-comment-body">
-                                        پیشنهاد میشه، کتاب مفیدیه
-                                    </section>
-                                </section>
 
-                                <section class="product-comment">
-                                    <section class="product-comment-header d-flex justify-content-start">
-                                        <section class="product-comment-date">۲۱ مرداد ۱۴۰۰</section>
-                                        <section class="product-comment-title">علی محمدی</section>
-                                    </section>
-                                    <section class="product-comment-body">
-                                        هنوز مطالعه نکردم ولی از نظر چاپ و نشر و قيمت مناسب عالیه، کیفیت چاپ و جنسش عالیه با
-                                        تخفیفی که خورده قیمت ۱۳ تومن واقعا براش فوق العاده هست محتوای کتابم که اصلا نیاز به
-                                        تعریف نداره
-                                    </section>
-                                </section>
-
-                                <section class="product-comment">
-                                    <section class="product-comment-header d-flex justify-content-start">
-                                        <section class="product-comment-date">۲۱ مرداد ۱۴۰۰</section>
-                                        <section class="product-comment-title">حسین رحیمی دهنوی</section>
-                                    </section>
-                                    <section class="product-comment-body">
-                                        این کتاب رو هر کسی باید حداقل یکبار تو زندگیش بخونه واقعا کتاب خوبیه
-                                    </section>
-
+                                    @if($comment->parent_id)
                                     <section class="product-comment ms-5 border-bottom-0">
                                         <section class="product-comment-header d-flex justify-content-start">
-                                            <section class="product-comment-date">۲۱ مرداد ۱۴۰۰</section>
-                                            <section class="product-comment-title">ادمین</section>
+                                            <section class="product-comment-date">{{ \Morilog\Jalali\Jalalian::forge($comment->created_at)->format('%A, %d %B %y') }}</section>
+                                            <section class="product-comment-title">{{ $comment->user->name }}</section>
                                         </section>
                                         <section class="product-comment-body">
-                                            این کتاب برای همه مفیده
+                                            {{ $comment->body }}
                                         </section>
                                     </section>
+                                    @endif
 
                                 </section>
+                                @endforeach
 
 
                             </section>
