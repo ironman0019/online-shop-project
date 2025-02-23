b2894bf37d9224108d4078fbb58647780e4df471
Emad Isavi <emadesavi@gmail.com>
2025-02-23 13:51:56 +0330

    Add product increment view_count and add comments query for passing it to the product view in product function, add and implement createComment function in home controller

--- app/Http/Controllers/Home/HomeControler.php
+++ app/Http/Controllers/Home/HomeControler.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\Home;
 
 use App\Http\Controllers\Controller;
 use App\Models\AdsBanner;
+use App\Models\Comment;
 use App\Models\Market\Brand;
 use App\Models\Market\Product;
 use App\Models\ShowCase;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeControler extends Controller
 {
@@ -30,7 +32,30 @@ class HomeControler extends Controller
 
     public function product(Product $product)
     {
+        $product->increment('view_count');
+
+        $comments = Comment::with('user')->where('commentable_type', get_class($product))->where('commentable_id', $product->id)->where('status', 2)->get();
+
         $relatedProducts = Product::where('marketable', 1)->where('product_category_id', $product->product_category_id)->inRandomOrder()->take(8)->get()->except($product->id);
-        return view('app.product', compact('product', 'relatedProducts'));
+
+        return view('app.product', compact('product', 'relatedProducts', 'comments'));
+    }
+
+    public function createComment(Request $request, Product $product)
+    {
+        $inputs = $request->validate([
+            'body' => 'required|string|min:4|max:255'
+        ]);
+
+        $user = Auth::user();
+
+        Comment::create([
+            'body' => $inputs['body'],
+            'user_id' => $user->id,
+            'commentable_id' => $product->id,
+            'commentable_type' => get_class($product)
+        ]);
+
+        return back()->with('success', 'کامنت ساخته و پس از تایید نمایش داده میشود');
     }
 }
