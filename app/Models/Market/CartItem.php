9d2049665e8073953d258981d849b07e0031329f
Emad Isavi <emadesavi@gmail.com>
2025-02-23 13:45:47 +0330

    Add relation with cart and product in cart item model

--- app/Models/Market/CartItem.php
+++ app/Models/Market/CartItem.php
@@ -10,4 +10,16 @@ class CartItem extends Model
     use SoftDeletes;
 
     protected $guarded = ['id'];
+
+    // Relation with cart
+    public function cart()
+    {
+        return $this->belongsTo(Cart::class);
+    }
+
+    // Relation with product
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
