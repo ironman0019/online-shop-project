f1326ae3d3165408684d1cee5469b70b9246aa34
Emad Isavi <emadesavi@gmail.com>
2025-02-24 20:50:17 +0330

    Add cartItems, user and coupan relations in cart model

--- app/Models/Market/Cart.php
+++ app/Models/Market/Cart.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Market;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -10,4 +11,23 @@ class Cart extends Model
     use SoftDeletes;
 
     protected $guarded = ['id'];
+
+    // Relation with cart items
+    public function cartItems()
+    {
+        return $this->hasMany(CartItem::class);
+    }
+
+    // Realtion with user
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // Relation with coupan
+    public function coupan()
+    {
+        return $this->belongsTo(Coupan::class);
+    }
+
 }
