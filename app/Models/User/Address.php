b5c96245a26dbe20e437b05d519ab8dc00a37316
Emad Isavi <emadesavi@gmail.com>
2025-02-26 00:23:58 +0330

    Add relation with user and city in address model

--- app/Models/User/Address.php
+++ app/Models/User/Address.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\User;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -10,4 +11,17 @@ class Address extends Model
     use SoftDeletes;
 
     protected $guarded = ['id'];
+
+    // Relation with user
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // Relation with city
+    public function city()
+    {
+        return $this->belongsTo(City::class);
+    }
+    
 }
