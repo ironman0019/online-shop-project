31ebd362f4f000ee4e178889dcfc7219e33a4640
Emad Isavi <emadesavi@gmail.com>
2025-02-26 00:23:28 +0330

    Add relation with addresses in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Models\User\Address;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -47,4 +49,12 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+
+    // Relation with addresses
+    public function addresses()
+    {
+        return $this->hasMany(Address::class);
+    }
+
 }
