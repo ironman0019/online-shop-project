9695bf705f6c8a500c0803dca2902e6b4096a56e
Emad Isavi <emadesavi@gmail.com>
2025-03-01 00:12:28 +0330

    Add relation with user, order and product in order item table

--- app/Models/Market/OrderItem.php
+++ app/Models/Market/OrderItem.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Market;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -10,4 +11,23 @@ class OrderItem extends Model
     use SoftDeletes;
 
     protected $guarded = ['id'];
+
+    // Relation with users table
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // Realtion with orders table
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    // Reltaion with products table
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
 }
