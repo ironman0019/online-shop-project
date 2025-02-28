5fa774e6f74c910cdb41fef781bff787dff010e0
Emad Isavi <emadesavi@gmail.com>
2025-03-01 00:17:04 +0330

    Add realtion with order items table and scope filter for filtering orders based on status and add status accessor in order model

--- app/Models/Market/Order.php
+++ app/Models/Market/Order.php
@@ -10,4 +10,36 @@ class Order extends Model
     use SoftDeletes;
 
     protected $guarded = ['id'];
+
+
+    // Relation with order items table
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    // Sort based on status filter
+    public function scopeFilter($query, array $filters)
+    {
+        if($filters['status'] ?? false) {
+            $query->where('order_status', request('status'));
+        }
+    }
+
+
+    // Get status
+    public function getStatusLabelAttribute()
+    {
+        $statuses = [
+            0 => 'در انتظار پرداخت',
+            1 => 'در حال پردازش',
+            2 => 'نحویل شده',
+            3 => 'لغو شده',
+            4 => 'مرجوعی',
+        ];
+
+        return $statuses[$this->order_status] ?? 'Unknown';
+    }
+
+
 }
